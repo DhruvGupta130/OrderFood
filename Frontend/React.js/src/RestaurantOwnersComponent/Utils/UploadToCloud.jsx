3b9bbd86a7b29951e9af146491a7105be5136736
Dhruv Gupta <89991050+DhruvGupta130@users.noreply.github.com>
2024-09-28 18:58:24 +0530

    Update UploadToCloud.jsx

--- Frontend/React.js/src/RestaurantOwnersComponent/Utils/UploadToCloud.jsx
+++ Frontend/React.js/src/RestaurantOwnersComponent/Utils/UploadToCloud.jsx
@@ -1,16 +1,16 @@
-const upload_preset = "orderFood";
-const cloud_name = "dp6vhtzen";
-const api_url = `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`
+const upload_preset = "YOUR_UPLOAD_PRESET";
+const cloud_name = "YOUR_CLOUD_NAME";
+const api_url = `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`;
 
-export const uploadImageToCloud = async (file) =>{
-    const data=new FormData();
-    data.append("file",file);
-    data.append("upload_preset",upload_preset);
-    data.append("cloud_name",cloud_name);
+export const uploadImageToCloud = async (file) => {
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", upload_preset);
+    data.append("cloud_name", cloud_name);
 
-    const response = await fetch(api_url,{
-        method:"post",
-        body:data
+    const response = await fetch(api_url, {
+        method: "post",
+        body: data
     });
 
     const fileData = await response.json();
