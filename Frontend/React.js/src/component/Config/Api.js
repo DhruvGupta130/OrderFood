86aa68c5adb6f3d2c3e3e2b92b7514574738c4f3
Dhruv Gupta <89991050+DhruvGupta130@users.noreply.github.com>
2024-10-22 22:41:18 +0530

    Update Api.js

--- Frontend/React.js/src/component/Config/Api.js
+++ Frontend/React.js/src/component/Config/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
-
-export const API_URL=["http://${IP}:8080", "http://localhost:8080"]
+const IP = "YOUR_IP"
+export const API_URL=[`http://${IP}:8080`, "http://localhost:8080"]
 
 export const api=axios.create({
     baseURL:API_URL[1],
