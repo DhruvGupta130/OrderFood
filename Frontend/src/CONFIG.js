a9727334b7a923f2306420f115390c3b168654d9
dhruv <dhruvgupta130@gmail.com>
2025-03-15 22:00:36 +0530

    Issues fix

--- Frontend/src/CONFIG.js
+++ Frontend/src/CONFIG.js
@@ -1,5 +1,3 @@
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
 export const UPLOAD_PRESET = process.env.REACT_APP_UPLOAD_PRESET;
 export const CLOUD_NAME = process.env.REACT_APP_CLOUD_NAME;
-
-console.log(CLOUD_NAME);
